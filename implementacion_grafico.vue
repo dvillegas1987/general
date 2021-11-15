1bd68ffbde7f99792402b51e5f1c3ed0e3e81231
dvillegas1987 <54553583+dvillegas1987@users.noreply.github.com>
2021-11-15 18:52:12 -0300

    Modificación de implementación

--- implementacion_grafico.vue
+++ implementacion_grafico.vue
@@ -98,8 +98,93 @@ export default {
                 this.descripcion_cpu_free = objeto.descripcion;
             })
 
+            this.aplicar_tema()
+            this.iniciar_grafico()
+      
+        },
 
-            /*Highcharts.theme = {
+        iniciar_grafico()
+        {
+            const socket = io(this.equipo)
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'line',
+                    animation: Highcharts.svg,
+                    marginRight: 10,
+                    events: {
+                        load: function () {
+                       
+                                var series = this.series[0];
+                                socket.on('datos-cpu', function(num){
+                                    var x = (new Date()).getTime();
+                                    console.log(num.data)
+                                    series.addPoint([x, num.data], true, true);
+                                }); 
+
+                        }
+                    },
+                    
+                },
+
+                time: {
+                    useUTC: false
+                },
+
+                title: {
+                    text: 'Live random data'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150
+                },
+                yAxis: {
+                    title: {
+                        text: 'Uso CPU porcentual (%)'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 100,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br/>',
+                    pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}%'
+                },
+                legend: {
+                    enabled: false
+                },
+                exporting: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Random data',
+                    data: (function () {
+                       
+                        var data = [],
+                            time = (new Date()).getTime(),
+                            i;
+
+                        for (i = -19; i <= 0; i += 1) {
+
+                            var y = null;
+                            socket.on('datos-cpu', function(num){
+                                y = num.data;
+                            })
+                            data.push({
+                                x: time + i * 1000,
+                                y: y
+                            });
+                            
+                        }
+                        return data;
+                    }())
+                }]
+            });
+        },
+
+        aplicar_tema(){
+            Highcharts.theme = {
                 colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
                     '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
                 chart: {
@@ -293,84 +378,7 @@ export default {
                 }
             };
             // Apply the theme
-            Highcharts.setOptions(Highcharts.theme);*/
-
-           Highcharts.chart('container', {
-                chart: {
-                    type: 'line',
-                    animation: Highcharts.svg,
-                    marginRight: 10,
-                    events: {
-                        load: function () {
-                       
-                                var series = this.series[0];
-                                socket.on('datos-cpu', function(num){
-                                    var x = (new Date()).getTime();
-                                    console.log(num.data)
-                                    series.addPoint([x, num.data], true, true);
-                                }); 
-
-                        }
-                    },
-                    
-                },
-
-                time: {
-                    useUTC: false
-                },
-
-                title: {
-                    text: 'Live random data'
-                },
-                xAxis: {
-                    type: 'datetime',
-                    tickPixelInterval: 150
-                },
-                yAxis: {
-                    title: {
-                        text: 'Uso CPU porcentual (%)'
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 100,
-                        color: '#808080'
-                    }]
-                },
-                tooltip: {
-                    headerFormat: '<b>{series.name}</b><br/>',
-                    pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}%'
-                },
-                legend: {
-                    enabled: false
-                },
-                exporting: {
-                    enabled: false
-                },
-                series: [{
-                    name: 'Random data',
-                    data: (function () {
-                       
-                        var data = [],
-                            time = (new Date()).getTime(),
-                            i;
-
-                        for (i = -19; i <= 0; i += 1) {
-
-                            var y = null;
-                            socket.on('datos-cpu', function(num){
-                                y = num.data;
-                            })
-                            data.push({
-                                x: time + i * 1000,
-                                y: y
-                            });
-                            
-                        }
-                        return data;
-                    }())
-                }]
-            });
-
+            Highcharts.setOptions(Highcharts.theme);
         }
 
     },
